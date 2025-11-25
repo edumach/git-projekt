3e32e11d51ee973e855fef14d46eafbfeea6b8d2
Jiří Machač <jirimachac@gmail.com>
2025-11-25 08:49:08 +0100

    Změna velikosti ....

--- kosticky.py
+++ kosticky.py
@@ -4,7 +4,7 @@ canvas = tkinter.Canvas()
 canvas.pack()
 
 def kostka():
-    canvas.create_rectangle(20,20,30,30)
+    canvas.create_rectangle(20,20,60,30)
 
 def kostka2():
     canvas.create_rectangle(50,50,50,50)
